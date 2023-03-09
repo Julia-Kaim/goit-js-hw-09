fa9affaf16eb78771c69b32990d0428b89bef63d
Julia-Kaim <114695828+Julia-Kaim@users.noreply.github.com>
2023-03-09 22:50:48 +0000

    Deploying to gh-pages from @ Julia-Kaim/goit-js-hw-09@bd9a91877df9eb4f6f9b097044d305c8d114b1de 🚀

---
+++ 01-color-switcher.687eecbc.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("data-start"),e=document.querySelector("data-stop"),n=null;t.addEventListener("click",(function(){n=setInterval((function(){}),1e3)})),e.addEventListener("click",(function(){clearInterval(n),console.log("Interval with id ".concat(n," has stopped!"))}))}();
+//# sourceMappingURL=01-color-switcher.687eecbc.js.map
